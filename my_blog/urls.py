d92023c7175089f3633504297a481c19bdd62c3e
quesiter <cuizhaoyang1995@163.com>
2018-05-11 15:26:09 +0800

    05111526

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 from article import views
+
 urlpatterns = [
 
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
+#    url(r'^admin/', admin.site.urls),
+#    url(r'^$', views.home),
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name = 'home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 ]
